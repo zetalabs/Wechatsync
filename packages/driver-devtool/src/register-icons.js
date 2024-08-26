62bdc9676065d595623ff56bea3156215225e514
david921518 <2456387@qq.com>
2024-08-26 11:43:05 +0800

    fix driver-devtool: address Can't resolve 'vue-awesome/components/Icon.vue' issue
    log:
    ERROR in ./src/register-icons.js 2:0-51
    Module not found: Error: Can't resolve 'vue-awesome/components/Icon.vue' in 'E:\MySource\Repos\github\zetalabs\Wechatsync\packages\driver-devtool\src'

--- packages/driver-devtool/src/register-icons.js
+++ packages/driver-devtool/src/register-icons.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Icon from 'vue-awesome/components/Icon.vue'
+import Icon from 'vue-awesome/components/Icon'
 import 'vue-awesome/icons/chevron-down'
 import 'vue-awesome/icons/plus'
 import 'vue-awesome/icons/folder-open'
