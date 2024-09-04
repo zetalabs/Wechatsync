660b76217dadc098f5c096e3924f750b69fbd17d
David唐辉 <david921518@qq.com>
2024-09-04 21:50:29 +0800

    fix driver-devtool: address build warnings

--- packages/driver-devtool/webpack.config.js
+++ packages/driver-devtool/webpack.config.js
@@ -25,6 +25,7 @@ module.exports = env => {
         }),
       ],
     },
+    performance: { hints: false },
   }
   const devConfigs = {
     mode: 'development',
@@ -37,6 +38,7 @@ module.exports = env => {
         vue: 'vue/dist/vue.js',
       },
     },
+    performance: { hints: false },
   }
   return merge(
     {
