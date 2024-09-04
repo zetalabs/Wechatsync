7bac40f68e83d329ff842c204584e07f344d76b8
david921518 <2456387@qq.com>
2024-09-04 12:35:58 +0800

    update all: remove mint-ui

--- packages/web-extension/src/popup.js
+++ packages/web-extension/src/popup.js
@@ -5,13 +5,11 @@ import EntryView from './views/EntryView.vue'
 import Option from './views/Option.vue'
 import AddAccount from './views/AddAccount.vue'
 import TaskDetail from './views/TaskDetail.vue'
-import Mint from 'mint-ui'
 import ElementUI from 'element-plus'
 import 'element-plus/theme-chalk/index.css'
 
 Vue.use(ElementUI)
 
-Vue.use(Mint)
 Vue.use(VueRouter)
 
 var routes = [
